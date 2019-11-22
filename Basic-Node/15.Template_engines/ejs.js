bc80b3af4e120f407148b112ed7667ed240e0960
guru prasath <guruprasath997@gmail.com>
2019-11-22 01:28:47 -0600

    Create ejs.js

---
+++ Basic-Node/15.Template_engines/ejs.js
@@ -0,0 +1,15 @@
+ let express = require('express');
+
+
+    let app = express();
+
+    app.set( 'view engine' , 'ejs');
+
+    app.get('/profile/:id',function(req,res)
+    {
+        var data = {age:29 , job : 'programmer'}
+        res.render('profile', {person:req.params.id, data : data})
+    });
+
+
+app.listen(3000);
